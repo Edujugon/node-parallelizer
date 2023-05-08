40e25219b7d8f60aaa0e126650d68c2bb45be4de
Eduardo Marcos <edujugon@gmail.com>
2023-05-08 14:20:05 +0200

    Update code examples

--- examples/with-bundler/src/lambda-parallelizer.js
+++ examples/with-bundler/src/lambda-parallelizer.js
@@ -3,17 +3,20 @@ const { Parallelizer, PARALLELIZER_CHILD, PARALLELIZER_THREADS } = require("node
 const parallelizerType = process.env.PARALLELIZER_TYPE || PARALLELIZER_CHILD;
 const parallelizerDebug = process.env.PARALLELIZER_DEBUG_MODE_ENABLED === 'true' ? true : false;
 
-const parallelizer = new Parallelizer({ type: parallelizerType, debug: parallelizerDebug, parallelizationPerCPU: process.env.PROCESSESPERCPU || 1 });
-
-// Creates child processes based with your custom code.
-parallelizer.parallelizerFunction({filePath: "/var/task/src/parallelizer-code-min.js", processBatchFunctionName: 'batchProcessor' });
+const parallelizer = new Parallelizer({
+  type: parallelizerType,
+  debug: parallelizerDebug,
+  parallelizationPerCPU: process.env.PROCESSESPERCPU || 1,
+  filePath: "/var/task/src/parallelizer-code-min.js",
+  processBatchFunctionName: 'batchProcessor'
+});
 
 exports.handler = async (event) => {
 
   const batch = [...Array(event.number).keys()];
 
   // Run batch in parallel
-  const responses = await parallelizer.runBatch(batch);
+  const responses = await parallelizer.run(batch);
   
   console.log(JSON.stringify(responses));
 };
